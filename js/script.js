75ec5c835ece7890507db9f0b733718f590a6a38
Gustavo <schaadtgustavo@gmail.com>
2022-01-25 20:10:17 -0300

    adicionando javascript e trocando o nome da página para shoot outlet

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function alertar(){
+    alert('vamos conversar ?');
+}
+
+function mensagem(){
+    var p = document.getElementsByTagName('p');
+    
+    p[0].innerHTML = "Não esqueça de avaliar a loja";   
+}
+
+function limpar(){
+    var p = document.getElementsByTagName('p');
+    
+    p[0].innerHTML = "";   
+}
